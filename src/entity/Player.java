3fa5b0002d2a81d16704df77a3154cb4515d045c
shakil-innovate <shakil.innovate@gmail.com.>
2025-05-16 08:41:43 +0600

    Update Player Class

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,11 +10,19 @@ import java.io.IOException;
 
 
 public class Player extends Entity{
+
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
+
     public Player(GamePanel gp,KeyHandler keyH)
     {
+        screenX=gp.screenWidth/2-(gp.tileSize)/2;
+        screenY=gp.screenHeight/2-(gp.tileSize)/2;
+
         this.gp=gp;
         this.keyH=keyH;
         setDefaultValue();
@@ -22,8 +30,8 @@ public class Player extends Entity{
     }
     public void setDefaultValue()
     {
-        x=100;
-        y=100;
+        worldX=gp.tileSize *23;
+        worldY=gp.tileSize * 21;
         speed=4;
         direction="down" ;
     }
@@ -50,16 +58,16 @@ public class Player extends Entity{
                 keyH.leftPressed==true || keyH.rightPressed == true) {
             if (keyH.upPressed == true) {
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             } else if (keyH.downPressed == true) {
                 direction = "down";
-                y += speed;
+                worldY += speed;
             } else if (keyH.leftPressed == true) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             } else if (keyH.rightPressed == true) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
             spiteCounter++;
             if (spiteCounter > 12) {
@@ -75,8 +83,8 @@ public class Player extends Entity{
         }
     public void draw(Graphics2D g2)
     {
-        g2.setColor(Color.white);
-        g2.fillRect(x,y,gp.tileSize,gp.tileSize);
+       // g2.setColor(Color.white);
+       // g2.fillRect(x,y,gp.tileSize,gp.tileSize);
         BufferedImage image =null;
 
         switch (direction)
@@ -109,7 +117,7 @@ public class Player extends Entity{
                     image=right2;
                 break;
         }
-        g2.drawImage(image,x,y,gp.tileSize,gp.tileSize,null);
+        g2.drawImage(image,screenX,screenY,gp.tileSize,gp.tileSize,null);
 
     }
 
