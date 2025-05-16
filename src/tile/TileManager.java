e31d032f5edb0b8fceb0809cebcfe54b6cb493dd
shakil-innovate <shakil.innovate@gmail.com.>
2025-05-16 12:28:41 +0600

    add collision

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -13,15 +13,15 @@ import java.io.InputStreamReader;
 public class TileManager {
 
     GamePanel gp;
-    Tile[] tile;
-    int[][] mapTileNum;
+    public Tile[] tile;
+    public int[][] mapTileNum;
 
     public TileManager(GamePanel gp)
     {
         this.gp=gp;
 
         tile= new Tile[10] ;
-        mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
+         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
         loadMap("/maps/world01.txt");
@@ -36,15 +36,18 @@ public class TileManager {
 
             tile[1] = new Tile();
             tile[1].image= ImageIO.read(getClass().getResourceAsStream("/TileImage/wall.png"));
+            tile[1].collision=true;
 
             tile[2] = new Tile();
             tile[2].image= ImageIO.read(getClass().getResourceAsStream("/TileImage/water.png"));
+            tile[2].collision=true;
 
             tile[3] = new Tile();
             tile[3].image= ImageIO.read(getClass().getResourceAsStream("/TileImage/earth.png"));
 
             tile[4] = new Tile();
             tile[4].image= ImageIO.read(getClass().getResourceAsStream("/TileImage/tree.png"));
+            tile[4].collision=true;
 
             tile[5] = new Tile();
             tile[5].image= ImageIO.read(getClass().getResourceAsStream("/TileImage/sand.png"));
