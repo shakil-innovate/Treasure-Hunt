5298da75348463025b54261ab793703160d71982
shakil-innovate <shakil.innovate@gmail.com.>
2025-05-17 15:37:33 +0600

    update boots

--- src/object/OBJ_Boots.java
+++ src/object/OBJ_Boots.java
@@ -1,17 +1,17 @@
-//package object;
-//
-//import javax.imageio.ImageIO;
-//import java.io.IOException;
-//
-//public class OBJ_Boots extends SuperObject{
-//
-//    public OBJ_Boots() {
-//        name = "Boots";
-//        try {
-//            image = ImageIO.read(getClass().getResourceAsStream("/Image/Object/boots.png"));
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Boots extends SuperObject{
+
+    public OBJ_Boots() {
+        name = "Boots";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/Resource/Object/boots.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
