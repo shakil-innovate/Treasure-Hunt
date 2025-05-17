3395d689c66d74391a1d469d9d411150701a45d9
shakil-innovate <shakil.innovate@gmail.com.>
2025-05-17 11:27:03 +0600

    add chest object

---
+++ src/object/OBJ_Chest.java
@@ -0,0 +1,17 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Chest extends SuperObject{
+
+    public OBJ_Chest() {
+        name = "Chest";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/Image/Object/chest.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
