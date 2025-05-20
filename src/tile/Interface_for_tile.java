42ecfd6d43fca2ec72725fc4f0f7a33b62ade6a5
shakil-innovate <shakil.innovate@gmail.com.>
2025-05-20 10:49:40 +0600

    Interface class

---
+++ src/tile/Interface_for_tile.java
@@ -0,0 +1,11 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+public class Interface_for_tile {           //interface class
+
+    public void loadMap(String filePath)
+    {
+
+    }
+}
